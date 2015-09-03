b50371caae819ee24a1d605cd5761dc8a7ba8b48
Ron Buckton <rbuckton@microsoft.com>
2015-09-03 13:54:50 -0700

    Updated version & headers, added .npmignore

--- reflect-metadata.d.ts
+++ reflect-metadata.d.ts
@@ -4,12 +4,13 @@ Licensed under the Apache License, Version 2.0 (the "License"); you may not use
 this file except in compliance with the License. You may obtain a copy of the 
 License at http://www.apache.org/licenses/LICENSE-2.0 
 
-Unless required by applicable law or agreed to in writing, software 
-distributed under the License is distributed on an "AS IS" BASIS, 
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE, 
+MERCHANTABLITY OR NON-INFRINGEMENT. 
 
-See the License for the specific language governing permissions and 
-limitations under the License. 
+See the Apache Version 2.0 License for specific language governing permissions
+and limitations under the License.
 ***************************************************************************** */
 declare module "reflect-metadata" { 
     // The "reflect-metadata" module has no imports or exports, but can be used by modules to load the polyfill.
