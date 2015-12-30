19cb40b74fea561875407890ead42b3c8b97f03f
Shadowgun <shadowgun@i.ua>
2015-12-30 21:58:50 +0200

    add msCrypto, Map, Set, WeakMap declaration

--- Reflect.ts
+++ Reflect.ts
@@ -12,13 +12,62 @@ MERCHANTABLITY OR NON-INFRINGEMENT.
 See the Apache Version 2.0 License for specific language governing permissions
 and limitations under the License.
 ***************************************************************************** */
-module Reflect {
+namespace Reflect {
     "use strict";
-    
+
+    interface Map<K, V> {
+        clear(): void;
+        delete(key:K): boolean;
+        forEach(callbackfn:(value:V, index:K, map:Map<K, V>) => void, thisArg?:any): void;
+        get(key:K): V;
+        has(key:K): boolean;
+        set(key:K, value?:V): Map<K, V>;
+        size: number;
+    }
+    interface MapConstructor {
+        new (): Map<any, any>;
+        new <K, V>(): Map<K, V>;
+        prototype: Map<any, any>;
+    }
+
+    interface WeakMap<K, V> {
+        clear(): void;
+        delete(key: K): boolean;
+        get(key: K): V;
+        has(key: K): boolean;
+        set(key: K, value?: V): WeakMap<K, V>;
+    }
+
+    interface WeakMapConstructor {
+        new (): WeakMap<any, any>;
+        new <K, V>(): WeakMap<K, V>;
+        prototype: WeakMap<any, any>;
+    }
+
+    interface Set<T> {
+        add(value: T): Set<T>;
+        clear(): void;
+        delete(value: T): boolean;
+        forEach(callbackfn: (value: T, index: T, set: Set<T>) => void, thisArg?: any): void;
+        has(value: T): boolean;
+        size: number;
+    }
+
+    interface SetConstructor {
+        new (): Set<any>;
+        new <T>(): Set<T>;
+        prototype: Set<any>;
+    }
+
+
+    declare var Set: SetConstructor;
+    declare var WeakMap: WeakMapConstructor;
+    declare var Map:MapConstructor;
     declare var global: any;
     declare var WorkerGlobalScope: any;
     declare var module: any;
     declare var crypto: Crypto;
+    declare var msCrypto: Crypto;
     declare var require: Function;
 
     // Load global or shim versions of Map, Set, and WeakMap
